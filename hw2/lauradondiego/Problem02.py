234f9d7bfee31a00ac6a9f35ec4cda317172d2d8
Carter Adams <carteradams@protonmail.com>
2018-10-24 10:43:19 -0400

    clean up repo some

--- hw2/lauradondiego/Problem02.py
+++
@@ -1,8 +0,0 @@
-hours = input("How many hours are you working? ")
-rate = input("At what rate of pay? ")
-Hoursworked = float(hours)
-rate = float(rate)
-total = rate * Hoursworked
-print(total)
-
-              
